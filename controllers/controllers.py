ffb28524471d13a2a5a17a7efcb56527d71d2b1e
Marcelo Paes Rocha <marcelo.rocha@nextsolucoes.net.br>
2019-02-07 20:06:57 -0300

    alteração controller

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -18,7 +18,7 @@ class Academy(http.Controller):
 #    def teacher(self, id):
 #        return '<h1>{} ({})</h1>'.format(id, type(id).__name__)
 
-    @http.route('/academy/<model("academy.teachers"):teacher>/', auth="public", website=True)
+    @http.route('/academy/<model("academy.teachers"):teacher>/', auth='public', website=True)
     def teacher(self, teacher):
         return http.request.render('academy.biography',{
             'person': teacher
